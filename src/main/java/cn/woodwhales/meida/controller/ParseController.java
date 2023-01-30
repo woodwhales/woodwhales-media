f646278b3136fcda1c010bf0faf8d6c796b0484a
woodwhales <woodwhales@163.com>
2023-01-30 11:40:46 +0800

    added saveOrUpdate function

--- src/main/java/cn/woodwhales/meida/controller/ParseController.java
+++ src/main/java/cn/woodwhales/meida/controller/ParseController.java
@@ -4,6 +4,8 @@ import cn.woodwhales.common.model.vo.RespVO;
 import cn.woodwhales.meida.model.dto.MediaInfoDto;
 import cn.woodwhales.meida.model.param.ParseParam;
 import cn.woodwhales.meida.service.ParseMediaService;
+import com.alibaba.fastjson.JSON;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -13,6 +15,7 @@ import org.springframework.web.bind.annotation.RestController;
 /**
  * @author woodwhales on 2023-01-29 11:53
  */
+@Slf4j
 @RestController
 @RequestMapping("/api")
 public class ParseController {
@@ -28,6 +31,12 @@ public class ParseController {
     @PostMapping("/parse")
     public RespVO<MediaInfoDto> parse(@RequestBody ParseParam param) {
         return RespVO.resp(parseMediaService.parseMovie(param));
+    }
+
+    @PostMapping("/saveOrUpdate")
+    public RespVO<Void> saveOrUpdate(@RequestBody MediaInfoDto mediaInfoDto) {
+        log.info("mediaInfoDto={}", JSON.toJSONString(mediaInfoDto));
+        return RespVO.success();
     }
 
 }
